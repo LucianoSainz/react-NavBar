02df56f3aacbd1eee1b74bea8bd18b4dbfacbabc
LucianoSainz <danielsainz28@gmail.com>
2021-06-21 11:37:16 +0200

    added folfer and files pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,6 +9,29 @@ function NavBar(){
                     <NavLink exct to="/" className='nav-logo'>
                         LucianoSainz
                     </NavLink>
+                    
+                    <ul>
+                        <li>
+                        <NavLink exct to="/" className='nav-links'>
+                            Home
+                        </NavLink>
+                        </li>
+                        <li>
+                        <NavLink exct to="/about" className='nav-links'>
+                            About
+                        </NavLink>
+                        </li>
+                        <li>
+                        <NavLink exct to="/portfolio" className='nav-links'>
+                            Portfolio
+                        </NavLink>
+                        </li>
+                        <li>
+                        <NavLink exct to="/contact" className='nav-links'>
+                            Contact
+                        </NavLink>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </>
